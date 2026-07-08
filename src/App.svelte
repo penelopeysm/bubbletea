9b8acf5f77b91b2dcfeeaef94d692082336555b1
Penelope Yong <penelopeysm@gmail.com>
2026-07-08 19:08:20 +0100

    Whew... that's all the T4s

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,20 @@
     import { LngLat } from "maplibre-gl";
     const charingCross = new LngLat(-0.127641, 51.507478);
     const defaultZoom = 11;
-
+    import { FontAwesomeIcon } from "@fortawesome/svelte-fontawesome";
+    import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
     import { shops } from "./shops";
 
     function displayDateOrString(d: Date | string | null): string {
         if (d instanceof Date) {
-            return d.toDateString();
+            // "7 Mar 2025 (Sat)"
+            const day = d.toLocaleDateString("en-GB", {
+                day: "numeric",
+                month: "short",
+                year: "numeric",
+            });
+            const weekday = d.toLocaleDateString("en-GB", { weekday: "short" });
+            return `${day} (${weekday})`;
         } else if (d === null) {
             return "Never";
         } else {
@@ -31,24 +39,32 @@
                 </span>
 
                 <Popup
-                    offset={[0, -10]}
+                    offset={[0, -25]}
                     onopen={() =>
                         map.flyTo({
                             center: shop.lngLat,
-                            zoom: Math.max(12, map.getZoom()),
-                            speed: 0.5,
+                            zoom: Math.max(defaultZoom, map.getZoom()),
+                            speed: 0.3,
                         })}
                 >
                     <div class="popup-content">
                         <div class="popup-header">
                             <h3>{shop.name}</h3>
-                            <span><a href={shop.address}>Google Maps</a></span>
+                            <span
+                                ><a href={shop.address} target="_blank"
+                                    >Google Maps
+                                    <FontAwesomeIcon
+                                        icon={faArrowUpRightFromSquare}
+                                        size="1x"
+                                    />
+                                </a></span
+                            >
                         </div>
                         <p>
                             Last visit: {displayDateOrString(shop.lastVisited)}
                         </p>
                         {#if shop.comments}
-                            <p>{shop.comments}</p>
+                            <p class="popup-comments">{shop.comments}</p>
                         {/if}
                     </div>
                 </Popup>
@@ -59,15 +75,27 @@
 
 <style>
     :global(.maplibregl-popup-content) {
-        padding: 0px 0px !important;
+        padding: 8px 8px !important;
+        border-radius: 5px;
+        border: 3px solid #505050;
+        background-color: #eaeaea;
+        max-width: 300px;
+    }
+    :global(.maplibregl-popup-tip) {
+        border-top-color: #505050 !important;
     }
 
     span.marker {
         font-size: 2.5rem;
     }
 
-    div.popup-content > p {
-        margin: 0;
+    div.popup-content {
+        * {
+            margin: 0;
+        }
+        p.popup-comments {
+            margin-top: 10px;
+        }
     }
 
     div.popup-header {
